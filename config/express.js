095b403d760ce6c318af15162164be676866d7b2
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2024-07-29 15:19:15 +0300

    making my express configuration

---
+++ config/express.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { create: handlebars } = require('express-handlebars');
+const session = require('express-session');
+
+module.exports = (app) => {
+
+    app.engine('.hbs', handlebars({
+        extname: '.hbs',
+    }).engine);
+    app.set('view engine', '.hbs')
+
+    //loaded my css file
+    app.use('/static', express.static('static'))
+
+    // use session
+    app.use(session({
+        secret: 'secret',
+        resave: false,
+        saveUninitialized: true,
+        cookie: {
+            secure: 'auto'
+        }
+    }))
+    app.use(express.urlencoded({ extended: true }));
+}

13b9c956d2198599ed00e2c5c774f541c0476a0b
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2024-07-30 12:54:03 +0300

    required schema

--- models/User.js
+++ models/User.js
@@ -2,10 +2,35 @@ const { Schema, model } = require('mongoose');
 
 // TODO change user model according to exam description
 // TODO add validation
+
+const NAME_PATTERN = /^[a-zA-Z-]+$/
+const EMAIL_PATTERN = /^([a-zA-Z]+)@([a-zA-Z]+)\.([a-zA-Z-]+)$/
+
 const userSchema = new Schema({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true },
+    firstName: {
+        type: String, minlength: [3, 'First name must be at least 3 characters long'], validate: {
+            validator(value) {
+                return NAME_PATTERN.test(value)
+            },
+            message: 'First name may contain only english letters'
+        }
+    },
+    lastName: {
+        type: String, minlength: [5, 'Last name must be at least 3 characters long'], validate: {
+            validator(value) {
+                return NAME_PATTERN.test(value)
+            },
+            message: 'Last name may contain only english letters'
+        }
+    },
+    email: {
+        type: String, required: [true, 'Email is required'], validate: {
+            validator(value) {
+                return EMAIL_PATTERN.test(value)
+            },
+            message: 'Email may contain only english letters'
+        }
+},
     hashedPassword: { type: String, required: true }
 });
 

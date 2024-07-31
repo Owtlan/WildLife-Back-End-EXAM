c5a265af351e378415541bd8bc5719f06cdff477
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2024-07-31 16:04:36 +0300

    WildLife exam done

--- controllers/home.js
+++ controllers/home.js
@@ -16,13 +16,14 @@ router.get('/catalog', async (req, res) => {
 router.get('/catalog/:id', async (req, res) => {
     const id = req.params.id;
     const post = postViewModel(await getPostById(id))
+  
 
     if (req.session.user) {
         post.hasUser = true
         if (req.session.user._id == post.author._id) {
             post.isAuthor = true;
         } else {
-            post.hasVoted = post.votes.includes(req.session.user._id)
+            post.hasVoted = post.votes.find(v => v._id == req.session.user._id) != undefined
         }
     }
 
